c64d5379d604bf38d4bfe4448a3a8668713ced0f
Chris Vanderplank <chris@intrepid-developer.com>
2023-07-27 11:13:01 +0100

    JWT Helper Class

---
+++ src/MobilityWays/MobilityWays.Application/Helpers/JwtHelper.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MobilityWays.Application.Helpers;
+
+//JWT Helper class to create and decode JWTs 
+public class JwtHelper
+{
+    //Static class to generate the Jwt with the name and email claims
+    public static string GenerateJwt(string name, string email, string secretKey)
+    {
+        var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Name, name),
+                new Claim(JwtRegisteredClaimNames.Email, email)
+            };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.Now.AddHours(24),
+            signingCredentials: creds
+        );
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    //Static class to validate a jwt
+    public static bool IsTokenValid(string jwt, string secretKey)
+    {
+        var handler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        handler.ValidateToken(jwt, new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        }, out var token);
+
+        return token != null;
+    }
+
+    //static class to decode a jwt giving a tuple back of the name and email
+    public static (string name, string email) DecodeJwt(string jwt, string secretKey)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secretKey);
+
+        tokenHandler.ValidateToken(jwt, new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ClockSkew = TimeSpan.Zero
+        }, out var validatedToken);
+
+        var jwtToken = (JwtSecurityToken)validatedToken;
+
+        var claims = jwtToken.Claims;
+
+        (string name, string email) user = new(claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Name)?.Value ?? string.Empty,
+                                                claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Email)?.Value ?? string.Empty);
+
+        return user;
+    }
+}
